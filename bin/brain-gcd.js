7fb251c3ed9548a5d3e5679a5810f814c7951b8f
Holerik <a_zobnin@mail.ru>
2023-02-08 20:29:01 +0300

    Игра 'НОД'

---
+++ bin/brain-gcd.js
@@ -0,0 +1,36 @@
+// brain-gcd.js
+import playGame from '../src/index.js';
+import { randomIntFromInterval } from '../src/index.js';
+
+const gameName = 'Find the greatest common divisor of given numbers.';
+
+const answerIsValid = (answer) => {
+  return !isNaN(+answer);
+}
+
+const generateQuestion = () => {
+  const value1 = randomIntFromInterval(1, 75);
+  const value2 = randomIntFromInterval(1, 25);
+  let max = value1 > value2 ? value1 : value2;
+  let min = value1 > value2 ? value2 : value1;
+  let res = max % min;
+  while (res > 0) {
+    max = min;
+    min = res;
+    res = max % min;
+  }
+  const rightAnswer = `${min}`;
+  return {
+    expression: `${value1} ${value2}`,
+    rightAnswer
+  };
+}
+
+function brainGcd()
+{
+  playGame(gameName, generateQuestion, answerIsValid);
+};
+
+brainGcd();
+
+export default brainGcd;
